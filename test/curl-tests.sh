3e022a832d775ca886b6fe3912ec5d8044eec4cd
jasonmimick <jmimick@gmail.com>
2013-06-27 09:35:49 -0400

    wiki stuff and more tests

---
+++ test/curl-tests.sh
@@ -0,0 +1,5 @@
+curl -X GET http://_system:SYS@localhost:57778/csp/user/$1?$2 
+#file="$1"
+#curl -sX POST --data-binary @$file http://_system:SYS@localhost:57778/csp/user/myblog.post.cls --header "Content-Type:application/json; charset=utf-8"
+
+
